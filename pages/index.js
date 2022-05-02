531fd7690f5688c03d852a807465cb0aa2568188
Eriquy Jhordan <eriquy@gmail.com>
2022-05-02 18:46:54 -0300

    chore: start application origin with nextjs

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
