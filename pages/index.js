143daff1379223e8315947e16e2a9f6cc543d1db
Eriquy Jhordan <eriquy@gmail.com>
2022-05-02 19:01:44 -0300

    chore: reset css propeties

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,12 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className={styles.title}>Welcome to Next.js!</h1>
+    </>
   );
 }
